42656118e10664057a213d7a51bc91e50c456c91
vipulmalik07 <chvipulmalik07@gmail.com>
2024-08-18 16:34:43 +0530

    added a edit functionality

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express();
 const port = 3000;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
+var methodOverride = require('method-override')
+
 
 //path set
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 
 
@@ -32,7 +36,7 @@ let posts = [
     },
 ]
 
-//api
+//apis---->
 
 
 //api for viewing all posts
@@ -54,6 +58,32 @@ app.get("/posts/:id", (req, res) => {
 })
 
 
+//api for creating a new post
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body
+    let id = uuidv4()
+    posts.push({ username, content, id })
+    res.redirect("/posts")
+})
+
+//api for loading page of  editing the post
+app.get('/posts/:id/edit', (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id)
+    res.render("edit.ejs", { post })
+
+})
+
+//api for patching the post 
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id)
+    post.content = newContent
+    res.redirect("/posts")
+    
+})
 
 
 
